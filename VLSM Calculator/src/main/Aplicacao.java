f99198b2ff058f241bbcbce3c4a71ceb722d05de
luuucaaas29 <lucas.camargo@live.com>
2017-11-15 20:58:24 -0200

    Commit inicial - VLSM Calculator
    Java VLSM Calculator
    Calculadora VLSM em Java

---
+++ VLSM Calculator/src/main/Aplicacao.java
@@ -0,0 +1,135 @@
+package main;
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.text.MaskFormatter;
+import javax.swing.JButton;
+import javax.swing.JFormattedTextField;
+
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.JTable;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import main.ControlIp;
+import javax.swing.JSpinner;
+import javax.swing.JTextArea;
+
+public class Aplicacao {
+
+	private JFrame frame;
+	private JTextField textField_1;
+	private JTable hosts;
+	
+	ControlIp control = ControlIp.getInstance();
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Aplicacao window = new Aplicacao();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Aplicacao() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 644, 433);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+//		txtIpBase = new JTextField();
+		MaskFormatter F_Mascara = new MaskFormatter();
+		try {
+			F_Mascara.setMask("###.###.###.###"); // Atribui a mascara
+			F_Mascara.setPlaceholderCharacter('0');
+		} catch (Exception excecao) {
+			excecao.printStackTrace();
+		}
+		JFormattedTextField txtIpBase = new JFormattedTextField(F_Mascara);
+		
+		txtIpBase.setToolTipText("");
+		txtIpBase.setBounds(10, 11, 204, 20);
+		frame.getContentPane().add(txtIpBase);
+		txtIpBase.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(10, 215, 99, 20);
+		frame.getContentPane().add(textField_1);
+		textField_1.setColumns(10);
+		
+		DefaultTableModel model = new DefaultTableModel();
+		model.addColumn("Quantidade");
+		hosts = new JTable(model);
+		hosts.setBounds(10, 42, 204, 162);
+		frame.getContentPane().add(hosts);
+
+		JSpinner spinner = new JSpinner();
+		spinner.setBounds(10, 249, 99, 20);
+		frame.getContentPane().add(spinner);
+		
+		JSpinner spinner_1 = new JSpinner();
+		spinner_1.setBounds(10, 283, 99, 20);
+		frame.getContentPane().add(spinner_1);
+		
+		JTextArea logUser = new JTextArea();
+		logUser.setBounds(224, 11, 394, 292);
+		frame.getContentPane().add(logUser);
+
+		JButton btnNewButton = new JButton("Adicionar");
+		btnNewButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				String quantidadeHost = textField_1.getText();
+				model.addRow(new Object[]{quantidadeHost});
+			}
+		});
+		btnNewButton.setBounds(119, 214, 95, 23);
+		frame.getContentPane().add(btnNewButton);
+		
+		JButton btnCalcular = new JButton("Subnet");
+		btnCalcular.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				String ipBase = txtIpBase.getText();
+				Integer subnet = (Integer) spinner.getValue();
+				
+				String ipBinary = control.avaliaIP(ipBase, subnet);
+				logUser.append(ipBinary);
+//				logUser.append(control.discoverClass(ipBase));
+//				logUser.append(control.ipToBinary(ipBase));
+			}
+		});
+		btnCalcular.setBounds(119, 248, 95, 23);
+		frame.getContentPane().add(btnCalcular);
+		
+		JButton btnCalcularPorHosts = new JButton("Hosts");
+		btnCalcularPorHosts.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnCalcularPorHosts.setBounds(119, 282, 95, 23);
+		frame.getContentPane().add(btnCalcularPorHosts);
+		
+		
+		
+	}
+}
---
+++ VLSM Calculator/src/main/ControlIp.java
@@ -0,0 +1,179 @@
+package main;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+
+public class ControlIp {
+	private static final ControlIp control = new ControlIp();
+
+	public static ControlIp getInstance() {
+		return control;
+	}
+	
+//	public String discoverClass(String ip) {
+//		String classe = "IP Classe ";
+//		String[] ipParts = ip.split("\\.");
+//		Integer octeto1 = Integer.parseInt(ipParts[0]);
+//		
+//		if (octeto1 >= 1 && octeto1 <= 127) {
+//			classe = classe + "A"
+//					+ "\nPrivate Address Space: 10.0.0.0 to 10.255.255.255"
+//					+ "\nDefault Subnet Mask: 255.0.0.0";
+//		}else if(octeto1 >= 128 && octeto1 <= 191) {
+//			classe = classe + "B"
+//					+ "\nPrivate Address Space: 172.16.0.0 to 172.31.255.255"
+//					+ "\nDefault Subnet Mask: 255.255.0.0";
+//		}else if(octeto1 >= 192 && octeto1 <= 223) {
+//			classe = classe + "C"
+//					+ "\nPrivate Address Space: 192.168.0.0 to 192.168.255.255"
+//					+ "\nDefault Subnet Mask: 255.255.255.0";
+//		}else if(octeto1 >= 224 && octeto1 <= 239) {
+//			classe = classe + "D";
+//		}else {
+//			classe = classe + "E";
+//		}
+//		
+//		return classe;
+//	}
+	public String[] discoverClass(String ip) {
+		String[] classe = new String[3];
+		String[] ipParts = ip.split("\\.");
+		Integer octeto1 = Integer.parseInt(ipParts[0]);
+		
+		if (octeto1 >= 1 && octeto1 <= 127) {
+			classe[0] = "A";
+			classe[1] = "255.0.0.0";
+			classe[2] = addressToBinary(classe[1]);
+		}else if(octeto1 >= 128 && octeto1 <= 191) {
+			classe[0] = "B";
+			classe[1] = "255.255.0.0";
+			classe[2] = addressToBinary(classe[1]);
+		}else if(octeto1 >= 192 && octeto1 <= 223) {
+			classe[0] = "C";
+			classe[1] = "255.255.255.0";
+			classe[2] = addressToBinary(classe[1]);
+		}else if(octeto1 >= 224 && octeto1 <= 239) {
+			classe[0] = "D";
+		}else {
+			classe[0] = "E";
+		}
+		return classe;
+	}
+	
+	public String binaryToAddress(String binary) {
+		
+		ArrayList<String> strings = new ArrayList<String>();
+		int index = 0;
+		while (index < binary.length()) {
+		    strings.add(binary.substring(index, Math.min(index + 8,binary.length())));
+		    index += 8;
+		}
+		
+		String octeto1 = "" + Integer.parseInt(strings.get(0), 2);
+		String octeto2 = "" + Integer.parseInt(strings.get(1), 2);
+		String octeto3 = "" + Integer.parseInt(strings.get(2), 2);
+		String octeto4 = "" + Integer.parseInt(strings.get(3), 2);
+		
+		return octeto1 + "." + octeto2 + "." + octeto3 + "." + octeto4;
+	}
+	
+	public String addressToBinary(String ip) {
+
+		String[] ipParts = ip.split("\\.");
+
+		String octeto1 = Integer.toBinaryString(Integer.parseInt(ipParts[0]));
+		String octeto2 = Integer.toBinaryString(Integer.parseInt(ipParts[1]));
+		String octeto3 = Integer.toBinaryString(Integer.parseInt(ipParts[2]));
+		String octeto4 = Integer.toBinaryString(Integer.parseInt(ipParts[3]));
+		
+		octeto1 = String.format("%08d", Integer.parseInt(octeto1));
+		octeto2 = String.format("%08d", Integer.parseInt(octeto2));
+		octeto3 = String.format("%08d", Integer.parseInt(octeto3));
+		octeto4 = String.format("%08d", Integer.parseInt(octeto4));
+		
+		return octeto1 + octeto2 + octeto3 + octeto4;
+	}
+	
+	public static int nextPowerOf2(final int a){
+		int b = 1;
+		while (b < a)
+		{
+		    b = b << 1;
+		}
+		return b;
+    }
+	
+	public boolean validateAddress(String address) {
+		
+		String[] addressParts = address.split("\\.");
+
+		if (Integer.parseInt(addressParts[0]) > 255 ||
+			Integer.parseInt(addressParts[1]) > 255 ||
+			Integer.parseInt(addressParts[2]) > 255 ||
+			Integer.parseInt(addressParts[3]) > 255) {
+			return false;
+		}
+		return true;
+	}
+
+	public String concatTexto(String texto, String concat) {
+		
+		return texto + "\n" + concat;
+	}
+	
+	public String addSubnet(String maskBinary, int subnet) {
+		int potencia = nextPowerOf2(subnet);
+		int potCount;
+		
+		for (potCount = 1; potCount <= 16; potCount++) {
+			potencia = potencia / 2;
+			if (potencia == 1)
+				break;
+		}
+		
+		for (int i = 0; i< 32; i++) {
+			if (maskBinary.charAt(i) == '0') {
+				maskBinary = maskBinary.substring(0,i)+'1'+maskBinary.substring(i+1);
+				potCount = potCount - 1;
+				if (potCount == 0) {
+					break;
+				}
+			}
+		}
+		
+		return maskBinary;
+	}
+	
+	public String avaliaIP(String ip, Integer subnet) {
+		String text = "\n";
+		
+		if (!validateAddress(ip)) {
+			return "IP Invalido";
+		}
+		
+		
+		String[] classe = discoverClass(ip);
+		String ipBinary = addressToBinary(ip);
+		
+		text = "Classe " + classe[0];
+		text = concatTexto(text, "IP: " + ip);
+		text = concatTexto(text, "Default Subnet Mask: " + classe[1]);
+		text = concatTexto(text, ipBinary);
+		text = concatTexto(text, classe[2]);
+		
+		BigInteger b1 = new BigInteger(ipBinary, 2);
+		BigInteger b2 = new BigInteger(classe[2], 2);
+		
+		String bitwise = "" + b1.and(b2).toString(2);
+//		text = concatTexto(text, bitwise);
+
+		String id = binaryToAddress(bitwise);
+		text = concatTexto(text, "\nID de Rede: " + id);
+
+		String customMask = addSubnet(classe[2], subnet);
+		customMask = binaryToAddress(customMask);
+		text = concatTexto(text, "\nCustom Subnet Mask: " + customMask);
+
+		return text;
+	}
+}
